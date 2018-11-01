fe3ef74b9ec8f435dbe416de67a5bc2f258b095e
github username <github email address>
2018-11-01 17:30:01 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Post
+
+  attr_accessor :title, :author
+  @@all =[]
+
+  def initialize(title)
+    @title = title
+    @author = author
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if @author
+      author.name
+    else
+      nil
+    end
+  end
+
+
+end
+
+#pry.start
